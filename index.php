83c37d581906d89871f7ddce42d9b7ea620554a8
dszidonia <szidonia.duduj@gmail.com>
2018-06-05 15:09:03 +0300

    Added config.php Added login/registration pages Added footer.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php ?>
+<?php 
+    //Allow the config
+    define('__CONFIG__',true);
+    //Require the config
+    require_once "inc/config.php"; 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,38 +27,18 @@
 
 <body>
     <div class="uk-section uk-container">
-        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-            <form class="uk-form-stacked js-login">
-
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">Email</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-                    </div>
-                </div>
-
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">Password</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your password">
-                    </div>
-                </div>
-
-                <div class="uk-margin">
-                    <button class="uk-button uk-button-default" type="submit">Login</button>
-                </div>
-
-            </form>
-        </div>
+        <?php
+            echo "hello world. Today is: ";
+            echo date("Y m d");
+        ?>
+        <p>
+            <a href="/Section16_Login_Registration/php_login_course/login.php">Login</a>
+            <a href="/Section16_Login_Registration/php_login_course/register.php">Register</a>
+        </p>
     </div>
 
+    <?php require_once "inc/footer.php"; ?>
 
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.5/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.5/js/uikit-icons.min.js"></script>
 </body>
 
 </html>
