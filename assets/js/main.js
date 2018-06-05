d1fd0cebc8c7dbfa6787968065bdc3bc3522aa33
dszidonia <szidonia.duduj@gmail.com>
2018-06-05 23:56:51 +0300

    Created User Login via AJAX

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,105 @@
-$(document).on("submit", "form.js-register", function (event) {
-    event.preventDefault();
+$(document)
+    //FOR REGISTER 
+    .on("submit", "form.js-register", function (event) {
+        event.preventDefault();
 
-    var _form = $(this);
-    var _error = $(".js-error", _form);
-    var dataObj = {
-        email: $("input[type='email']", _form).val(),
-        password: $("input[type='password']", _form).val()
-    };
+        var _form = $(this);
+        var _error = $(".js-error", _form);
+        var dataObj = {
+            email: $("input[type='email']", _form).val(),
+            password: $("input[type='password']", _form).val()
+        };
 
-    //Validating email and password length
-    if (dataObj.email.length < 6) {
-        _error
-            .text("Please enter a valid email address!")
-            .show();
+        //Validating email and password length
+        if (dataObj.email.length < 6) {
+            _error
+                .text("Please enter a valid email address!")
+                .show();
+            return false;
+        } else if (dataObj.password.length < 11) {
+            _error
+                .text("Please enter a passphrase that is at least 11 characters long!")
+                .show();
+            return false;
+        }
+
+        //Assuming the code gets this far, we can start the ajax process
+        _error.hide();
+
+        $.ajax({
+                type: 'POST',
+                url: '/Section16_Login_Registration/php_login_course/ajax/register.php',
+                data: dataObj,
+                dataType: 'json',
+                async: true
+            })
+            .done(function ajaxDone(data) {
+                if (data.redirect !== undefined) {
+                    window.location = data.redirect;
+                } else if (data.error !== undefined) {
+                    _error
+                        .text(data.error)
+                        .show();
+                }
+            })
+            .fail(function ajaxFailed(e) {
+
+            })
+            .always(function ajaxAlwaysDoThis(data) {
+
+            })
+
         return false;
-    } else if (dataObj.password.length < 11) {
-        _error
-            .text("Please enter a passphrase that is at least 11 characters long!")
-            .show();
+    })
+    //FOR LOGIN
+    .on("submit", "form.js-login", function (event) {
+        event.preventDefault();
+
+        var _form = $(this);
+        var _error = $(".js-error", _form);
+        var dataObj = {
+            email: $("input[type='email']", _form).val(),
+            password: $("input[type='password']", _form).val()
+        };
+
+        //Validating email and password length
+        if (dataObj.email.length < 6) {
+            _error
+                .text("Please enter a valid email address!")
+                .show();
+            return false;
+        } else if (dataObj.password.length < 11) {
+            _error
+                .text("Please enter a passphrase that is at least 11 characters long!")
+                .show();
+            return false;
+        }
+
+        //Assuming the code gets this far, we can start the ajax process
+        _error.hide();
+
+        $.ajax({
+                type: 'POST',
+                url: '/Section16_Login_Registration/php_login_course/ajax/login.php',
+                data: dataObj,
+                dataType: 'json',
+                async: true
+            })
+            .done(function ajaxDone(data) {
+                if (data.redirect !== undefined) {
+                    window.location = data.redirect;
+                } else if (data.error !== undefined) {
+                    _error
+                        .text(data.error)
+                        .show();
+                }
+            })
+            .fail(function ajaxFailed(e) {
+
+            })
+            .always(function ajaxAlwaysDoThis(data) {
+
+            })
+
         return false;
-    }
-
-    //Assuming the code gets this far, we can start the ajax process
-    _error.hide();
-
-    $.ajax({
-            type: 'POST',
-            url: '/Section16_Login_Registration/php_login_course/ajax/register.php',
-            data: dataObj,
-            dataType: 'json',
-            async: true
-        })
-        .done(function ajaxDone(data) {
-            if (data.redirect !== undefined) {
-                window.location = data.redirect;
-            } else if (data.error !== undefined) {
-                _error
-                    .text(data.error)
-                    .show();
-            }
-        })
-        .fail(function ajaxFailed(e) {
-
-        })
-        .always(function ajaxAlwaysDoThis(data) {
-
-        })
-
-    return false;
-})
+    })
